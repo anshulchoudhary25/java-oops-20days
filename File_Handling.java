4d3760f2801323801aab5e96b7b260c9455bd0d5
Anshul Choudhary <anshulchoudhary2005@gmail.com>
2026-04-10 22:35:47 +0530

    File handling

--- File_Handling.java
+++ File_Handling.java
@@ -7,19 +7,19 @@ public class File_Handling{
             fw.write("My name is Anshul Choudhary \nI'm studying file Handling in java \nthis is my 4th day of this streak");
             fw.close();
 
-            /*FileReader fr = new FileReader("test.txt");
+            FileReader fr = new FileReader("test.txt");
             int ch;
             while ((ch = fr.read()) != -1) {
                 System.out.print((char)ch);         
             }
-            fr.close();*/
+            fr.close();
 
-            BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+            /*BufferedReader br = new BufferedReader(new FileReader("test.txt"));
             String s;
             while ((s = br.readLine()) != null) {
                 System.out.println(s);
             }
-            br.close();
+            br.close();*/
 
         }
         catch(Exception e){
